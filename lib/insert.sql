4e69ff32bba0806db6b8d27a4e1fb6a3feea673b
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-04-15 11:24:49 +0300

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', NULL, 'M', 'brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', NULL, 'M', 'black', 'aggressive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', NULL, 'F', 'gray', 'playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', NULL, 'M', 'brown', 'serious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', NULL, 'F', 'white', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', NULL, 'M', 'green', 'mischievous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', NULL, 'F', 'brown', 'energetic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
